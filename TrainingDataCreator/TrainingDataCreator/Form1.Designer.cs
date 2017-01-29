3f7984ab155e5a4016ebc43c826e38af18a53d0b
fleth <fleth@users.noreply.github.com>
2017-01-29 21:18:48 +0900

    * ビルド設定の変更  * 終了ボタンのホットキー文言を修正

--- TrainingDataCreator/TrainingDataCreator/Form1.Designer.cs
+++ TrainingDataCreator/TrainingDataCreator/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "終了（F12）";
+            this.button2.Text = "終了（Ctrl+F11）";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
